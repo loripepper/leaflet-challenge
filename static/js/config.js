02804af02b2dc7e11a03ce76edc88972e512993d
loripepper <82188849+loripepper@users.noreply.github.com>
2021-10-15 12:43:50 -0700

    First push

---
+++ static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoibG9yaXBlcHBlciIsImEiOiJja3VuN2hhb2szeW1uMnBxNmg2bzFnOG5iIn0.f67A4lHT0ZrKppSobxtQtA";
---
+++ static/js/logic.js
@@ -0,0 +1,81 @@
+var queryUrl = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
+
+d3.json(queryUrl).then (function(data) {
+    console.log(data.features);
+
+
+var streetmap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+  id: 'mapbox/streets-v11',
+	accessToken: API_KEY
+});
+
+var darkmap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+  id: 'mapbox/dark-v10',
+	accessToken: API_KEY
+});
+
+var baseMaps = {
+    "Street Map": streetmap,
+    "Dark Map": darkmap
+  };
+
+var myMap = L.map("mapid", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5,
+    layers: [streetmap]
+  });
+
+var earthquakes = new L.layerGroup();  
+  
+
+L.geoJSON(data, {
+    pointToLayer: function (feature, latlng) {
+      return L.circleMarker(latlng)
+
+    },
+    style: function (feature) {
+      return {
+        fillColor: getColor(feature.properties.mag),
+        color: "white",
+        opacity:1,
+        fillOpacity:0.5,
+        radius: feature.properties.mag * 6
+      }
+    }
+  }).addTo(myMap);
+
+  function getColor(magnitude) {
+    if (magnitude > 5) {
+      return "red";
+    }
+    if (magnitude > 4) {
+      return "orange";
+    }
+    if (magnitude > 3) {
+      return "gold";
+    }
+    if (magnitude > 2) {
+      return "yellow";
+    }
+    if (magnitude > 1) {
+      return "lightyellow";
+    }
+    return "lime";
+  };
+  
+
+var overlayMaps = {
+  "Earthquakes": earthquakes
+};
+
+streetmap.addTo(myMap);
+L.control.layers(baseMaps, overlayMaps, {collapsed: true})
+.addTo(myMap);
+
+}); 
